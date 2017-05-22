8704e1d1f5e6f875fb170414816349c12fb7670e
Weslley Almeida <weslley.almd@hotmail.com>
2017-05-22 17:21:54 -0300

    productss CRUD

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,23 +1,17 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
-  # GET /clients
-  # GET /clients.json
   def index
     @clients = Client.all
   end
 
-  # GET /clients/new
   def new
     @client = Client.new
   end
 
-  # GET /clients/1/edit
   def edit
   end
 
-  # POST /clients
-  # POST /clients.json
   def create
     @client = Client.new(client_params)
 
@@ -32,8 +26,6 @@ class ClientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
   def update
     respond_to do |format|
       if @client.update(client_params)
@@ -46,8 +38,6 @@ class ClientsController < ApplicationController
     end
   end
 
-  # DELETE /clients/1
-  # DELETE /clients/1.json
   def destroy
     @client.destroy
     respond_to do |format|
@@ -57,12 +47,10 @@ class ClientsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_client
       @client = Client.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
       params.require(:client).permit(
         :name
